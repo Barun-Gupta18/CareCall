cf1b9615c60ca93cbfc9d429692cc30579429aa4
Barun Gupta <gvarun9814@gmail.com>
2024-11-29 18:57:45 +0530

    ...

--- client/utils/config.js
+++ client/utils/config.js
@@ -1,4 +1,4 @@
 // export const Server_URL = 'http://localhost:5000/';
-export const Server_URL = 'http://localhost:10000/';
-export const Server_URL2 = 'http://localhost:10000';
+export const Server_URL = 'https://care-call-backend-wn32.onrender.com/';
+export const Server_URL2 = 'https://care-call-backend-wn32.onrender.com';
 export const rapid_api = '0db1ff55c7mshcaa2c13a194506ep1ecab8jsnd5b96d259f1c';
