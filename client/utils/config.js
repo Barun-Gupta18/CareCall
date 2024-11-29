8c6b5e8b9df97f1f35d75f8e6de3484bfaa4c1ce
Barun Gupta <gvarun9814@gmail.com>
2024-11-29 18:21:23 +0530

    changes for deploying frontend

--- client/utils/config.js
+++ client/utils/config.js
@@ -1,4 +1,4 @@
 // export const Server_URL = 'http://localhost:5000/';
-export const Server_URL = 'https://care-call-backend-5vh5.onrender.com/';
+export const Server_URL = 'https://care-call-backend-5vh5.onrender.com';
 export const Server_URL2 = 'http://localhost:5000';
 export const rapid_api = '0db1ff55c7mshcaa2c13a194506ep1ecab8jsnd5b96d259f1c';
